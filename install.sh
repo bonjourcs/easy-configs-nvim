09661d275f0866bb6ef6524a3dc89fffb381322c
Liang Chenghao <lch_office@foxmail.com>
2022-07-15 09:04:18 +0800

    update: install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 NVIM_GIT_REPOSITORY=https://github.com/bonjourcs/easy-configs-nvim.git
 PACKER_GIT_REPOSITORY=https://github.com/wbthomason/packer.nvim
-NVIM_MODULE_DIR=nvim
-USER_CONFIG_DIR=~/.config
+USER_NVIM_CONFIG_DIR=~/.config/nvim
 PCAKER_DIR=~/.local/share/nvim/site/pack/packer/start/packer.nvim
 timestamp=$(date +%s)
 
-if [ -d $NVIM_MODULE_DIR ] || [ -f $NVIM_MODULE_DIR ]; then
-	mv $NVIM_MODULE_DIR $NVIM_MODULE_DIR.$timestamp
-fi
-
-if [ -d $USER_CONFIG_DIR/$NVIM_MODULE_DIR ]; then
-	mv $USER_CONFIG_DIR/$NVIM_MODULE_DIR $USER_CONFIG_DIR/$NVIM_MODULE_DIR.$timestamp
+if [ -d  $USER_NVIM_CONFIG_DIR]; then
+	mv $USER_NVIM_CONFIG_DIR $USER_NVIM_CONFIG_DIR.$timestamp
 fi
 
 if [ -d $PCAKER_DIR ]; then
 	mv $PCAKER_DIR $PCAKER_DIR.$timestamp
 fi
 
-git clone $NVIM_GIT_REPOSITORY $NVIM_MODULE_DIR && mv $NVIM_MODULE_DIR $USER_CONFIG_DIR/
+git clone $NVIM_GIT_REPOSITORY $USER_CONFIG_DIR/$NVIM_MODULE_DIR 
 
 git clone $PACKER_GIT_REPOSITORY $PCAKER_DIR 
 
